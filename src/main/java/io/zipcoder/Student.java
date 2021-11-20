c189f53bfa5196e819895e62f45910fb2dfe8f74
junior <junior@zipcodes-MacBook-Pro-5.local>
2021-11-19 23:58:55 -0500

    almost completed student class and test

--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -1,4 +1,61 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Student {
+    private String firstName;
+    private String lastName;
+    private int NumberOfExamsTaken;
+    private ArrayList<Double> examScores;
+
+
+    public Student(String firstName, String lastName, Double[] testScores) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.examScores = new ArrayList<>(Arrays.asList(testScores));
+    }
+
+  public String setExamScore(int examNumber, double newScore){
+       examScores.set(examNumber -1, newScore);
+       return ("Exam " + examNumber + " -> " + examScores.get(examNumber - 1));
+
+    }
+
+
+    public String addExamScore(double examScore) {
+        examScores.add(examScore);
+
+        return "Exam 1 -> " + examScores.get(0);
+    }
+
+    public String getTestScores() {
+        String testScores = "";
+        for (int i = 0; i < examScores.size(); i++) {
+            testScores = ("Exam " + (i + 1) + " -> " + examScores.get(i) + "\n");
+        }
+
+        return testScores;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+
+    public ArrayList<Double> getExamScores() {
+        return examScores;
+    }
 }
--- src/test/java/io/zipcoder/StudentTest.java
+++ src/test/java/io/zipcoder/StudentTest.java
@@ -1,5 +1,61 @@
 package io.zipcoder;
 
+import org.junit.Test;
+
 public class StudentTest {
+
+    public static void main(String[] args) {
+
+    }
+
+    @Test
+    public void getTestScoreTest() {
+        // : Given
+        String firstName = "Leon";
+        String lastName = "Hunter";
+        Double[] examScores = {100.0, 95.0, 123.0, 96.0};
+        Student student = new Student(firstName, lastName, examScores);
+
+// When
+        String output = student.getTestScores();
+
+// Then
+        System.out.println(output);
+
+    }
+
+@Test
+    public void addExamScoreTest(){
+    // : Given
+    String firstName = "Leon";
+    String lastName = "Hunter";
+    Double[] examScores = {};
+    Student student = new Student(firstName, lastName, examScores);
+
+    // When
+    student.addExamScore(100.0);
+    String output = student.getTestScores();
+
+     // Then
+     System.out.println(output);
+}
+
+@Test
+    public void setExamScoreTest(){
+    // : Given
+    String firstName = "Leon";
+    String lastName = "Hunter";
+    Double[] examScores = { 100.0 };
+    Student student = new Student(firstName, lastName, examScores);
+
+// When
+    student.setExamScore(1, 150.0);
+    String output = student.getTestScores();
+
+// Then
+    System.out.println(output);
+
+
+}
 
 }
